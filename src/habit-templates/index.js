6480d0fd82d4cdcd460203625d64ce2b5d4045ec
Andrii Doroshenko <andry.dorosh@gmail.com>
2025-10-01 13:46:38 +0300

    update genration of template habit

--- src/habit-templates/index.js
+++ src/habit-templates/index.js
@@ -1,3 +1,10 @@
 export function generateTemplateHabit() {
-    return `Do lab works`;
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < 12; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return result;
 }
